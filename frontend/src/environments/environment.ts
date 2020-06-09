8b09a2de964a3aa184ee27ab90e1a7cc76e76c92
Jessica <yutsuc@gmail.com>
2020-06-09 11:55:56 -0700

    feat: Update frontend env variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'jchenapp', // the auth0 domain prefix
+    audience: 'http://127.0.0.1:5000', // the audience set for the auth0 app
+    clientId: 'wn0YTywP7nzyoW4RDJ8koCc71on9Dfhg', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
