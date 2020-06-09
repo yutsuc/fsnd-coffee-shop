1657b2f33858ac3b8aef473331264f30396377d5
Jessica <yutsuc@gmail.com>
2020-06-09 13:20:12 -0700

    fix: Update Auth to correctly abort and fix typos

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -1,5 +1,5 @@
 import json
-from flask import request, _request_ctx_stack
+from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
@@ -52,11 +52,13 @@ def get_token_auth_header():
     elif len(parts) == 1:
         raise AuthError({
             "code": "invalid_header",
-            "description": "Token not found"}, 401)
-    elif len(parts) >= 2:
+            "description": "Token not found"
+        }, 401)
+    elif len(parts) > 2:
         raise AuthError({
             "code": "invalid_header",
-            "description": "Authorization header must be Bearer token"}, 401)
+            "description": "Authorization header must be Bearer token"
+        }, 401)
 
     return parts[1]
 
@@ -75,13 +77,14 @@ return true otherwise
 
 
 def check_permissions(permission, payload):
-    if "permission" not in payload:
+    print("check permissions")
+    if "permissions" not in payload:
         raise AuthError({
             "code": "invalid_claims",
             "description": "Permissions not included in JWT"
         }, 401)
     
-    if permission not in payload["permission"]:
+    if permission not in payload["permissions"]:
         raise AuthError({
             "code": "unauthorized",
             "description": "Permission not found"
@@ -108,7 +111,7 @@ return the decoded payload
 def verify_decode_jwt(token):
     auth_url = f"https://{AUTH0_DOMAIN}/.well-known/jwks.json"
     jsonurl = urlopen(auth_url)
-    jwks = json.loads(jsonurl)
+    jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
     rsa_key = {}
 
@@ -121,7 +124,7 @@ def verify_decode_jwt(token):
                 "n": key["n"],
                 "e": key["e"]
             }
-
+    
     if rsa_key:
         try:
             payload = jwt.decode(
@@ -169,10 +172,15 @@ def requires_auth(permission=''):
     def requires_auth_decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
-            token = get_token_auth_header()
-            payload = verify_decode_jwt(token)
-            check_permissions(permission, payload)
-            return f(payload, *args, **kwargs)
-
+            try:
+                token = get_token_auth_header()
+                payload = verify_decode_jwt(token)
+                check_permissions(permission, payload)
+                _request_ctx_stack.top.current_user = payload
+            except AuthError as e:
+                abort(e.status_code, description=e.error["description"])
+            except:
+                abort(401)
+            return f(*args, **kwargs)
         return wrapper
     return requires_auth_decorator
